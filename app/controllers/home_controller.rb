cfbec02bcc4465cb70c507cc7acd982390d42df5
kemister85 <karlkemistersheppard@gmail.com>
2022-08-22 12:57:22 +1000

    Adding home controller pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,21 +2,21 @@ class HomeController < ApplicationController
   def index
   end
 
-  # def about
-  # end
+  def about
+  end
 
-  # def contact
-  # end
+  def contact
+  end
 
-  # def journey
-  # end
+  def journey
+  end
 
-  # def sign_in
-  # end
+  def sign_in
+  end
 
-  # def sign_out
-  # end
+  def sign_out
+  end
 
-  # def sign_up
-  # end
+  def sign_up
+  end
 end
